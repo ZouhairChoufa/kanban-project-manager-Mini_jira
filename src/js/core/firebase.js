325d35b487610cfaf66fc9a693aef437cf9a500e
ZouhairChoufa <149824783+ZouhairChoufa@users.noreply.github.com>
2025-11-18 16:09:44 +0100

    Remove Firebase config from code and use environment variables

--- src/js/core/firebase.js
+++ src/js/core/firebase.js
@@ -30,13 +30,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoc1m-IVplJRK6-1TLeKFK-meEAFECVEw",
-  authDomain: "mini-jira-kanban-board.firebaseapp.com",
-  projectId: "mini-jira-kanban-board",
-  storageBucket: "mini-jira-kanban-board.firebasestorage.app",
-  messagingSenderId: "706807233151",
-  appId: "1:706807233151:web:f4ca57d2a9604ab3871d4a",
-  measurementId: "G-STE06L3GV5"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 export const appId = firebaseConfig.projectId || 'default-kanban-app';
 
